23e40248142bdc56a559957718c584464046c5fc
mdHasif2001 <iamhasib727@gmail.com>
2022-05-07 03:06:06 +0600

    firsh commit

---
+++ _server_deploy.js
@@ -0,0 +1,15 @@
+/* 
+--------------------------
+One Time for you computer
+--------------------------
+1. create heroky account
+2. verify email
+3. install heroku cli
+4. heroku login
+------------------------------
+for each project for one time
+------------------------------
+1. heroku create 
+2. make sure you: git add . git commit . git push
+git push heroku main
+ */
